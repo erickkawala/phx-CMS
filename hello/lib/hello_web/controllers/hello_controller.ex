b26594335f8dca51fb60313184b4d15888b01fd5
Erick Kawala <erickkawala@gmail.com>
2019-07-16 11:53:43 -0700

    Hello messenger URL / string matcher added. Add Hello>show view. Add HelloControllow#Show action

--- hello/lib/hello_web/controllers/hello_controller.ex
+++ hello/lib/hello_web/controllers/hello_controller.ex
@@ -1,7 +1,11 @@
 defmodule HelloWeb.HelloController do
-    use HelloWeb, :controller
+  use HelloWeb, :controller
 
-    def index(conn, _params) do
-        render(conn, "index.html")
-    end
+  def index(conn, _params) do
+    render(conn, "index.html")
+  end
+
+  def show(conn, %{"messenger" => messenger}) do
+    render(conn, "show.html", messenger: messenger)
+  end
 end

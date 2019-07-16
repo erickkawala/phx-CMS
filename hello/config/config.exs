1d98bbade4bd36663d8ab0564f06c4f3b12ccb71
Erick Kawala <erickkawala@gmail.com>
2019-07-16 11:53:43 -0700

    Hello messenger URL / string matcher added. Add Hello>show view. Add HelloControllow#Show action

--- hello/config/config.exs
+++ hello/config/config.exs
@@ -24,6 +24,8 @@ config :logger, :console,
 
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
+# javascript function :
+# json_library() returns phx json encoding library
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.

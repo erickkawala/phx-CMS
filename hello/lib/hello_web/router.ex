8341c098f0bef62d51e28665483651ee36f25b6a
Erick Kawala <erickkawala@gmail.com>
2019-07-16 20:18:31 -0700

    Add validations Author

--- hello/lib/hello_web/router.ex
+++ hello/lib/hello_web/router.ex
@@ -17,7 +17,7 @@ defmodule HelloWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
-    get "/", Hellontroller, :index
+    get "/", HelloController, :index
     get "/hello/:messenger", HelloController, :show
   end
 

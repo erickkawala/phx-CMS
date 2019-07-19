4318539d8f51fb30219970dce001ae447c49eefe
Erick Kawala <erickkawala@gmail.com>
2019-07-18 17:49:01 -0700

    Add preload methods for user_list, get_user

--- hello/lib/hello_web/controllers/page_controller.ex
+++ hello/lib/hello_web/controllers/page_controller.ex
@@ -4,4 +4,20 @@ defmodule HelloWeb.PageController do
   def index(conn, _params) do
     render(conn, "index.html")
   end
+  defp require_existing_author(conn, _) do
+      author = CMS.ensure_author_exists(conn.assigns.current_user)
+      assign(conn, :current_author, author)
+  end
+
+    defp authorize_page(conn, _) do
+      page = CMS.get_page!(conn.params["id"])
+
+      if conn.assigns.current_author.id == page.author_id do
+        assign(conn, :page, page)
+      else
+        conn
+        |> put_flash(:error, "You can't modify that page")
+        |> redirect(to: Routes.cms_page_path(conn, :index))
+        |> halt()
+    end
 end

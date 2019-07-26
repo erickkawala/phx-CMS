c2294e0910b5b6369d92d076997cb0eb3435eaba
Erick Kawala <erickkawala@gmail.com>
2019-07-25 21:33:48 -0700

    Author route, controllers, and view-template setup

--- lib/hello_web/controllers/cms/author_controller.ex
+++ lib/hello_web/controllers/cms/author_controller.ex
@@ -6,7 +6,7 @@ alias Hello.CMS.Author
 alias Hello.CMS.Page
 
   def index(conn, _params) do
-    pages = CMS.list_authors()
+    authors = CMS.list_authors()
     render(conn, "index.html", authors: authors)
   end
 
